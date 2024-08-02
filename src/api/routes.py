f2a31bd54acd9f210c441f5e8739abddcaa75ac3
Alba <allbaluccia@gmail.com>
2024-08-02 17:42:31 +0000

    cambios despues del git pull

--- src/api/routes.py
+++ src/api/routes.py
@@ -27,7 +27,6 @@ def handle_hello():
 @api.route('/experience_levels', methods=['POST'])
 def create_experience_level():
     data = request.get_json()
-    levels = ['beginner', 'intermediate', 'advanced']
     new_experience_level = ExperienceLevel(
         level_name=data['level_name'],
         user_id=data['user_id']
@@ -37,6 +36,7 @@ def create_experience_level():
     return jsonify({"message": "Experience level created successfully"}), 201
 
 @api.route('/experience_levels', methods=['GET'])
+@jwt_required()
 def get_experience_levels():
     experience_levels = ExperienceLevel.query.all()
     result = [
