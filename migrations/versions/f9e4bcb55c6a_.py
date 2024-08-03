601432c230d46ab10f5af93ccf7b19a6ec9a1976
i-lema <161314957+i-lema@users.noreply.github.com>
2024-08-03 19:13:28 +0000

    Reset

--- migrations/versions/f9e4bcb55c6a_.py
+++ migrations/versions/f9e4bcb55c6a_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: badada0184a0
+Revision ID: f9e4bcb55c6a
 Revises: 
-Create Date: 2024-08-02 17:55:58.166801
+Create Date: 2024-08-03 19:12:53.151400
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'badada0184a0'
+revision = 'f9e4bcb55c6a'
 down_revision = None
 branch_labels = None
 depends_on = None
